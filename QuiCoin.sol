52812cd5e885c300e4afcbc1f2033c325d9dabc5
QUIQQY <evandonald01@gmail.com>
2024-07-17 23:06:35 +0100

    Update QuiCoin.sol

--- QuiCoin.sol
+++ QuiCoin.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import "./@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "./@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
 import "./@openzeppelin/contracts/security/Pausable.sol";
 import "./@openzeppelin/contracts/access/Ownable.sol";
 import "./@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
